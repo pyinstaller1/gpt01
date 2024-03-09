853f2b19036f232ed7173b2cca80312ae0ba2eb7
pyinstaller1 <145110542+pyinstaller1@users.noreply.github.com>
2024-03-09 23:46:50 +0900

    Create main07.py

---
+++ main07.py
@@ -0,0 +1,16 @@
+
+from langchain.chat_models import ChatOpenAI
+import streamlit as st
+
+
+chat_model = ChatOpenAI(openai_api_key="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")
+
+
+st.title('건강보험 GPT')
+
+content = st.text_input('GPT 3.5 LLM 기반 챗봇... 질문하기 버튼을 누르세요.')
+
+if st.button('질문하기'):
+    with st.spinner('답변 작성 중...'):
+        result = chat_model.predict(content)
+        st.write(result)
