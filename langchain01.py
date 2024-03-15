43cc0094c2d74137047681c9470114a53c9e636e
pyinstaller1 <145110542+pyinstaller1@users.noreply.github.com>
2024-03-15 13:55:52 +0900

    Update langchain01.py

--- langchain01.py
+++ langchain01.py
@@ -83,3 +83,88 @@ demo.launch(debug=True)
 
 
 print(7)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+
+
+import gradio as gr
+
+def greet(name):
+  return "안녕! " + name
+
+demo = gr.Interface(
+    fn=greet,
+    inputs="text",
+    outputs="text"
+)
+
+demo.launch()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import gradio as gr
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import AIMessage, HumanMessage, SystemMessage
+
+chat_model = ChatOpenAI(openai_api_key="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")
+# result = chat_model([HumanMessage(content="안녕? 넌 누구니?")])
+
+def respond(message, chat_history):
+
+  result = chat_model([HumanMessage(content=message)])
+  
+  bot_message = result.content
+  # bot_message = chat_model.predict(message)
+
+  chat_history.append((message, bot_message))
+
+  return "", chat_history
+
+with gr.Blocks() as demo:
+  chatbot = gr.Chatbot(label="채팅창")
+  msg = gr.Textbox(label="입력")
+  clear = gr.Button("초기화")
+
+  msg.submit(respond, [msg, chatbot], [msg, chatbot])
+  clear.click(lambda: None, None, chatbot, queue=False)
+
+demo.launch(debug=True)
+
+"""
