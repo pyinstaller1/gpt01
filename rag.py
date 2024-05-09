7d9c16b08e33ceef6d2cc92c39e5f40bb287777e
pyinstaller1 <145110542+pyinstaller1@users.noreply.github.com>
2024-05-10 01:30:07 +0900

    Update rag.py

--- rag.py
+++ rag.py
@@ -95,8 +95,6 @@ def main():
                 st.markdown(response)
                 with st.expander("참고 문서 확인"):
                     st.markdown(source_documents[0].metadata['source'], help=source_documents[0].page_content)
-                    # st.markdown(source_documents[2].metadata['source'], help=source_documents[2].page_content)
-                    # st.markdown(source_documents[1].metadata['source'], help=source_documents[1].page_content)
 
         # Add assistant message to chat history
         st.session_state.messages.append({"role": "assistant", "content": response})
