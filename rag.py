ac29ccdb18417c7577fc103a3aa02081a66433de
pyinstaller1 <145110542+pyinstaller1@users.noreply.github.com>
2024-05-20 17:52:04 +0900

    Update rag.py

--- rag.py
+++ rag.py
@@ -1,9 +1,13 @@
+from langchain_openai import ChatOpenAI
+import os
 import streamlit as st
+
+
+
 import tiktoken
 from loguru import logger
 
 from langchain.chains import ConversationalRetrievalChain
-from langchain.chat_models import ChatOpenAI
 
 from langchain.document_loaders import PyPDFLoader
 from langchain.document_loaders import Docx2txtLoader
@@ -15,18 +19,15 @@ from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.memory import ConversationBufferMemory
 from langchain.vectorstores import FAISS
 
-# from streamlit_chat import message
 from langchain.callbacks import get_openai_callback
 from langchain.memory import StreamlitChatMessageHistory
 
 
 def main():
-    st.set_page_config(
-        page_title="챗봇",
-        page_icon=":robot_face:")
-
+    st.set_page_config(page_title="챗봇", page_icon=":robot_face:")
     st.title(":red[NHIS]&nbsp;_챗봇_  &nbsp;&nbsp;&nbsp; :robot_face:")
-    st.text("유튜브 보고 만들었음 (동영상 제목: Streamlit으로 RAG 시스템 구축하기)")
+    st.text("PDF 파일을 등록하시고 질문하세요.")
+
 
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
@@ -37,67 +38,80 @@ def main():
     if "processComplete" not in st.session_state:
         st.session_state.processComplete = None
 
-    openai_api_key = "sk-proj-RsnIPjHF8l7LXymJ2EdVT3BlbkFJbOLSUuJjJlXDNWBEJsZS"
 
     with st.sidebar:
         uploaded_files = st.file_uploader("PDF 자료를 여기에 첨부하세요.", type=['pdf', 'docx'], accept_multiple_files=True)
-        # openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
         process = st.button("첨부된 파일 등록")
 
-    process_check = False
-    if process:
+    if  len(uploaded_files) != 0:
 
-        files_text = get_text(uploaded_files)
-
-        st.info("파일 내용을 작은 Text 청크들로 쪼개고 있습니다...")
-
-
-        if 1== 1:
-            text_chunks = get_text_chunks(files_text)
-
-            st.info("숫자로 임베딩해서 벡터DB에 저장하고 있습니다...")
-
-            vetorestore = get_vectorstore(text_chunks)
-
-            st.session_state.conversation = get_conversation_chain(vetorestore, openai_api_key)
+        if process:   # 첨부파일 등록 버튼을 누른 경우
+            files_text = get_text(uploaded_files)            # PDF 파일 받기
+            message_info = st.info("파일을 벡터 DB에 저장하고 있습니다.")
+            text_chunks = get_text_chunks(files_text)      # 청크 문장 단위로 나누기
+            vetorestore = get_vectorstore(text_chunks)   # 벡터 DB 저장
+            key = os.getenv('OPENAI_KEY')
+            st.session_state.conversation = get_conversation_chain(vetorestore, key)   # 벡터DB 내용과 GPT LLM을 연결
             st.session_state.processComplete = True
+            message_info.empty()
 
-            st.info("벡터DB에 저장 완료")
-            process_check = True
+        history = StreamlitChatMessageHistory(key="chat_messages")
 
-    if 'messages' not in st.session_state:
-        st.session_state['messages'] = [{"role": "assistant",
-                                         "content": "질문하시면 친절하게 답변 드리겠습니다."}]
+    elif len(uploaded_files) == 0 and "messages" in st.session_state:
+        if process:
+            st.error("파일을 첨부하세요.")
 
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
 
-    history = StreamlitChatMessageHistory(key="chat_messages")
 
-    # Chat logic
-    if query := st.chat_input("질문을 입력해주세요."):
-        st.session_state.messages.append({"role": "user", "content": query})
 
-        with st.chat_message("user"):
-            st.markdown(query)
+    if "messages" not in st.session_state:
+        st.session_state["messages"] = [("assistant", "질문하시면 친절하게 답변 드리겠습니다.")]
 
-        with st.chat_message("assistant"):
-            chain = st.session_state.conversation
+    for chat_message in st.session_state.messages:
+        st.chat_message(chat_message[0]).write(chat_message[1])
 
-            with st.spinner("답변 생성 중..."):
-                result = chain({"question": query})
-                with get_openai_callback() as cb:
-                    st.session_state.chat_history = result['chat_history']
-                response = result['answer']
-                source_documents = result['source_documents']
+    question = st.chat_input("질문을 입력하세요.")
+    if question:
 
-                st.markdown(response)
-                with st.expander("참고 문서 확인"):
-                    st.markdown(source_documents[0].metadata['source'], help=source_documents[0].page_content)
+        if  len(uploaded_files) != 0:                # 첨부파일이 있으면, RAG
+            st.chat_message("user").write(question)
+            with st.chat_message("assistant"):
+                chain = st.session_state.conversation
+                with st.spinner("RAG 답변 생성 중..."):
+                    result = chain({"question": question})
+                    with get_openai_callback() as cb:
+                        st.session_state.chat_history = result['chat_history']
+                    answer = result['answer']
+                    st.write(answer)
 
-        # Add assistant message to chat history
-        st.session_state.messages.append({"role": "assistant", "content": response})
+                    source_documents = result['source_documents']
+                    with st.expander("참고 문서 확인"):
+                        st.markdown(source_documents[0].metadata['source'], help=source_documents[0].page_content)
+
+
+        if  len(uploaded_files) == 0:            # 첨부 파일이 없으면, 챗GPT 3.5
+            st.chat_message("user").write(question)
+
+
+            with st.spinner("챗GPT가 답변 생성 중..."):
+                key = os.getenv('OPENAI_KEY')
+                llm = ChatOpenAI(openai_api_key = key)
+                answer = llm.invoke(question)
+                answer = answer.content
+                st.chat_message("assistant").write(answer)
+    
+
+
+        st.session_state["messages"].append(["user", question])
+        st.session_state["messages"].append(["assistant", answer])
+
+
+
+
+
+
+
+
 
 
 def tiktoken_len(text):
@@ -147,11 +161,8 @@ def get_vectorstore(text_chunks):
     vectordb = FAISS.from_documents(text_chunks, embeddings)
     return vectordb
 
-
-
-
-def get_conversation_chain(vetorestore, openai_api_key):
-    llm = ChatOpenAI(openai_api_key=openai_api_key, model_name='gpt-3.5-turbo', temperature=0)
+def get_conversation_chain(vetorestore, key):
+    llm = ChatOpenAI(openai_api_key=key, model_name='gpt-3.5-turbo', temperature=0)
     conversation_chain = ConversationalRetrievalChain.from_llm(
         llm=llm,
         chain_type="stuff",
@@ -163,9 +174,6 @@ def get_conversation_chain(vetorestore, openai_api_key):
     )
 
     return conversation_chain
-
-
-
 
 def process_local_file(file_path):
     # 파일 확장자에 따라 적절한 로더 선택
@@ -183,4 +191,3 @@ def process_local_file(file_path):
 
 if __name__ == '__main__':
     main()
-
