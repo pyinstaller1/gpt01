99984eb14ee837a76cc28091e178f2547598e338
pyinstaller1 <145110542+pyinstaller1@users.noreply.github.com>
2024-05-29 15:34:04 +0900

    Update api01.py

--- api01.py
+++ api01.py
@@ -5,3 +5,25 @@ params ={'serviceKey' : '서비스키', 'YM' : '201201', 'NAT_CD' : '112', 'ED_C
 
 response = requests.get(url, params=params)
 print(response.content)
+
+
+
+
+
+
+import json
+import urllib.request
+
+key = "키"
+URL = "hrrp://www/key=" + key + "&data="
+json_page = urllib.request.urlopen(URL)
+json_data = json_page.read().decode("utf-8")
+json_array = json.loads(json_data)
+
+print(json_array)
+
+
+
+
+
+
