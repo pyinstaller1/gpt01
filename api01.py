5a8e6fd5727dd74df56cd38d74d77c149a76b0ae
pyinstaller1 <145110542+pyinstaller1@users.noreply.github.com>
2024-05-31 15:53:27 +0900

    Update api01.py

--- api01.py
+++ api01.py
@@ -23,6 +23,30 @@ json_array = json.loads(json_data)
 print(json_array)
 
 
+import requests
+from datetime import datetime
+
+
+
+
+
+
+
+
+url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst'
+params ={'serviceKey' : 'LeXut7UmcYum2fOusIxBEeUSjTyABxPbM2odYVlwkRYAWVCbsT/xx4bUIO52VxqMUO/yZUT60MwFoCdbQLr1sw==', 'pageNo' : '1', 'numOfRows' : '1000', 'dataType' : 'JSON', 'base_date' : datetime.now().strftime('%Y%m%d'), 'base_time' : datetime.now().strftime('%H%M'), 'nx' : '77', 'ny' : '122' }
+
+response = requests.get(url, params=params)
+
+print(response.content)
+print(datetime.now().strftime('%H%M'))
+
+response_json = response.json()
+
+for item in response_json["body"]["items"]["item"]:
+    if item["category"] == "T1H":
+        print("현재 기온:", item["obsrValue"], "°C")
+        break
 
 
 
